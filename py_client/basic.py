a4ebc2e1360b256a75c5dd0812f61a018e496453
danvidana <32367237+danvidana@users.noreply.github.com>
2023-03-02 12:17:15 -0600

    Created clients for tests

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,5 +2,5 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, json={"product_id": 123}) # HTTP Request
+get_response = requests.post(endpoint, json={"title": "Hello world"}) # HTTP Request
 print(get_response.json())
