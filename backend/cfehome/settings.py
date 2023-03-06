8919a1918de3cd94ff1280ad30d4bb0a1077f64e
danvidana <32367237+danvidana@users.noreply.github.com>
2023-03-05 20:46:00 -0600

    Remove a hardcoded Django secret key

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wfoa8!nf%)937%hj%nz%p#i^0^)$!m(r7^d$v7mbjs$i(0!riy'
+# SECRET_KEY = 'django-insecure-wfoa8!nf%)937%hj%nz%p#i^0^)$!m(r7^d$v7mbjs$i(0!riy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
